5c4e3e747e5d3f5dfc0e0fd739024feab7d99184
shaojunda <shaojunda@gmail.com>
2020-06-10 15:49:36 +0800

    chore: add some gems and config travis ci

--- Gemfile
+++ Gemfile
@@ -38,7 +38,6 @@ gem "mina-multistage", require: false
 # gem 'rack-cors'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "pry"
   gem "pry-nav"
 end
@@ -53,6 +52,18 @@ group :development do
   gem "rubocop-rails"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+end
+
+group :test do
+  gem "simplecov", require: false
+  gem "minitest-reporters"
+  gem "shoulda-context"
+  gem "shoulda-matchers"
+  gem "database_cleaner"
+  gem "mocha"
+  gem "factory_bot_rails"
+  gem "faker", git: "https://github.com/stympy/faker.git", branch: "master"
+  gem "codecov", require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,32 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/app/channels/"
+  add_filter "/app/jobs/"
+  add_filter "/app/mailers/"
+  add_filter "/lib/api/"
+end
+Minitest::Reporters.use!
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+
+DatabaseCleaner.strategy = :transaction
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :minitest
+
+    with.library :rails
+  end
+end
+
+if ENV["CI"] == "true"
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
