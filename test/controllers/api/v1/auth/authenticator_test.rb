cc4ba8eb0f63fc91c07908817bb34c787dedd865
shaojunda <shaojunda@gmail.com>
2020-06-12 02:19:00 +0800

    test: signed headers

--- test/controllers/api/v1/auth/authenticator_test.rb
+++ test/controllers/api/v1/auth/authenticator_test.rb
@@ -57,4 +57,16 @@ class AuthenticatorTest < ActiveSupport::TestCase
       authenticator.authenticate!
     end
   end
+
+  test "should raise error if signed headers is invalid" do
+    create(:product, access_key_id: "TYkNNrK4wjmche2i6WBAvajZ")
+    request = mock
+    headers = { host: "domain.com", authorization: "CKBFS1-HMAC-SHA256 Credential=TYkNNrK4wjmche2i6WBAvajZ/20200611/faucet/ckbfs1_request, SignedHeaders=host;x-ckbfs-date;x-ckbfs-content-sha256" }.stringify_keys
+    request.expects(:headers).returns(headers)
+    authenticator = Api::V1::Auth::Authenticator.new(request, {})
+
+    assert_raises Api::V1::ApiError::SignedHeadersInvalidError do
+      authenticator.authenticate!
+    end
+  end
 end
