869a88c46cb25e7805f9eb8d69d85b2ddddc6907
shaojunda <shaojunda@gmail.com>
2020-06-13 08:34:38 +0800

    feat: handle api error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :check_header_info
+  rescue_from Api::V1::ApiError, with: :api_error
+
+  private
+    def check_header_info
+      raise Api::V1::ApiError::ContentTypeInvalidError if request.headers["Content-Type"] != "application/vnd.api+json"
+      raise Api::V1::ApiError::AcceptInvalidError if request.headers["Accept"] != "application/vnd.api+json"
+    end
+
+    def api_error(error)
+      render json: ApiErrorSerializer.new([error], message: error.title), status: error.status
+    end
 end
