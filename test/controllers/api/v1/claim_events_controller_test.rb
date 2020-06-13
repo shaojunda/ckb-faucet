59034cf90f1a4c7578169d66effefdbc8a03b752
shaojunda <shaojunda@gmail.com>
2020-06-14 00:53:32 +0800

    test: should get uuid after claim event created

--- test/controllers/api/v1/claim_events_controller_test.rb
+++ test/controllers/api/v1/claim_events_controller_test.rb
@@ -167,4 +167,27 @@ class Api::V1::ClaimEventsControllerTest < ActionDispatch::IntegrationTest
 
     assert_equal response_json, response.body
   end
+
+  test "should get uuid after claim event created" do
+    product = create(:product, quota_config: { "h24_quota": 4, "h24_quota_per_request_type": 2 })
+    type_script_args = "0x94bbc8327e16d195de87815c391e7b9131e80419c51a405a0b21227c6ee05129"
+    pk160 = "0x69b7667edbe08cf19413102fcadc53c67e34fb71"
+    request_body = { data: { id: 1, type: "claim_event", attributes: {
+        request_uuid: type_script_args, request_type: 0, pk160: pk160
+    } } }.to_json
+    timestamp = Time.now.utc.strftime("%Y%m%dT%H%M%SZ")
+    request = mock
+    body = StringIO.new(request_body)
+    headers = { "x-ckbfs-date": timestamp, host: "domain.com" }.stringify_keys
+    request.expects(:headers).returns(headers).at_least_once
+    request.expects(:query_string).returns("").at_least_once
+    request.expects(:method).returns("POST").at_least_once
+    request.expects(:body).returns(body).at_least_once
+
+    valid_post api_v1_claim_events_url, params: request_body, headers: { "x-ckbfs-date": timestamp, "authorization": authorization(request, timestamp, product) }
+
+    expected_response = ClaimEventSerializer.new(ClaimEvent.last).serialized_json
+
+    assert_equal expected_response, response.body
+  end
 end
