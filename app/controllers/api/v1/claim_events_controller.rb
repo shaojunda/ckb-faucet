3ec8f7d5e6fcab61c27b69e50be1c60d0402e191
shaojunda <shaojunda@gmail.com>
2020-06-13 08:35:00 +0800

    feat: add empty create action

---
+++ app/controllers/api/v1/claim_events_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Api::V1::ClaimEventsController < ApplicationController
+  def create
+    render json: {}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :claim_events, only: %i(create)
+    end
+  end
 end
+
