34ced12594e597f8b5546972b9f489eee247096a
shaojunda <shaojunda@gmail.com>
2020-06-14 03:07:21 +0800

    feat: add create official account method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class Account < ApplicationRecord
+  def self.create_official_account
+    api = CKB::API.new(host: Rails.application.credentials.CKB_NODE_URL)
+    ckb_wallet = CKB::Wallet.from_hex(api, Rails.application.credentials.OFFICIAL_WALLET_PRIVATE_KEY)
+    api.index_lock_hash(ckb_wallet.lock_hash)
+    balance = api.get_capacity_by_lock_hash(ckb_wallet.lock_hash)
+    account = Account.find_or_create_by(address_hash: ckb_wallet.address)
+    account.update(balance: balance.capacity)
+  end
 end
 
 # == Schema Information
