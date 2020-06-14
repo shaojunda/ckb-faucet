e200692ef5a4e480839adb5e6368a0a4a5a00791
shaojunda <shaojunda@gmail.com>
2020-06-15 05:04:25 +0800

    feat: add split cell service

---
+++ app/services/split_cell_service.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+class SplitCellService
+  def call
+    api = CKB::API.new(host: Rails.application.credentials.CKB_NODE_URL)
+    official_account = Account.last
+    ckb_wallet = Wallet.new(api: api, from_addresses: official_account.address_hash, for_split: true)
+    CKB::Config.instance.set_api(Rails.application.credentials.CKB_NODE_URL)
+    balance = official_account.balance
+    output_balance = Output.where(status: %w(live)).sum(:capacity).to_i
+    cells_count = (balance - output_balance) / (207 * 10**8)
+    cells_count.times.each_slice(1500) do |items|
+      ActiveRecord::Base.transaction do
+        to_infos = items.map do
+          { "#{official_account.address_hash}": { capacity: 207 * 10**8 } }.stringify_keys
+        end
+        tx_generator = ckb_wallet.advance_generate(to_infos: to_infos)
+        tx = ckb_wallet.advance_sign(tx_generator: tx_generator, contexts: Rails.application.credentials.OFFICIAL_WALLET_PRIVATE_KEY)
+        tx_hash = api.send_transaction(tx)
+        split_cell = SplitCellEvent.create!(tx_hash: tx_hash)
+        save_previous_output(api, split_cell, tx)
+        save_output(split_cell, tx, tx_hash)
+      end
+    end
+  end
+
+  def check_transactions
+    api = CKB::API.new(host: Rails.application.credentials.CKB_NODE_URL)
+    loop do
+      SplitCellEvent.order(:id).where(status: "pending").each do |event|
+        tx = api.get_transaction(event.tx_hash)
+        if tx.tx_status.status == "committed"
+          ActiveRecord::Base.transaction do
+            block_hash = tx.tx_status.block_hash
+            block = api.get_block(block_hash)
+            event.block_hash = block_hash
+            event.block_number = block.header.number
+            event.completed!
+            event.outputs.live.update_all(block_hash: block_hash)
+            event.outputs.collected.update_all(status: "dead")
+          end
+        end
+      end
+      break if SplitCellEvent.order(:id).where(status: "pending").count.zero?
+
+      sleep(10)
+    end
+
+    puts "done"
+  end
+
+  private
+    def save_output(split_cell, tx, tx_hash)
+      output_values = tx.outputs.each_with_index.map do |output, index|
+        lock = output.lock
+        type = output.type
+        {
+            capacity: output.capacity, data: tx.outputs_data[index], split_cell_event_id: split_cell.id,
+            lock_args: lock.args, lock_code_hash: lock.code_hash, lock_hash: lock.compute_hash, lock_hash_type: lock.hash_type,
+            type_args: type&.args, type_code_hash: type&.code_hash, type_hash: type&.compute_hash, type_hash_type: type&.hash_type,
+            tx_hash: tx_hash, cell_index: index, created_at: Time.current, updated_at: Time.current
+        }
+      end
+      Output.upsert_all(output_values, unique_by: %i[tx_hash cell_index])
+    end
+
+    def save_previous_output(api, split_cell, tx)
+      previous_output_values = tx.inputs.map do |input|
+        previous_output = input.previous_output
+        tx_with_status = api.get_transaction(previous_output.tx_hash)
+        transaction = tx_with_status.transaction
+        cell_index = previous_output.index
+        output = transaction.outputs[cell_index]
+        lock = output.lock
+        type = output.type
+        {
+            capacity: output.capacity, data: transaction.outputs_data[cell_index], status: "collected", split_cell_event_id: split_cell.id,
+            lock_args: lock.args, lock_code_hash: lock.code_hash, lock_hash: lock.compute_hash, lock_hash_type: lock.hash_type,
+            type_args: type&.args, type_code_hash: type&.code_hash, type_hash: type&.compute_hash, type_hash_type: type&.hash_type,
+            tx_hash: transaction.hash, cell_index: cell_index, created_at: Time.current, updated_at: Time.current, block_hash: tx_with_status.tx_status.block_hash
+        }
+      end
+
+      Output.upsert_all(previous_output_values, unique_by: %i[tx_hash cell_index])
+    end
+end
