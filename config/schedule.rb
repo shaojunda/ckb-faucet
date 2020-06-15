4ba4ccc78f9b871bc5a058477367a71309074578
shaojunda <shaojunda@gmail.com>
2020-06-15 10:24:22 +0800

    chore: update config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -18,6 +20,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+set :output, { standard: "cron.log" }
 
 every 2.hours do
   runner "CheckOutputService.new.call"
