904b7d543b8863c956534f594099d277a2bc1aa7
shaojunda <shaojunda@gmail.com>
2020-06-16 00:13:46 +0800

    fix: upsert all bug

--- app/services/send_capacity_service.rb
+++ app/services/send_capacity_service.rb
@@ -11,12 +11,15 @@ class SendCapacityService
         lock_script = CKB::Types::Script.new(code_hash: CKB::Config::SECP256K1_BLAKE160_SIGHASH_ALL_TYPE_HASH, args: claim_event.pk160, hash_type: "type")
         target_address = CKB::Address.new(lock_script, mode: api.mode).generate
 
-        { "#{target_address}": { capacity: 145 * 10**8, type: SudtTypeScriptGenerator.new(claim_event.pk160).type_script, data: CKB::Utils.generate_sudt_amount(0) } }.stringify_keys
+        { "#{target_address}": { capacity: 145 * 10**8, type: SudtTypeScriptGenerator.new(claim_event.request_uuid).type_script, data: CKB::Utils.generate_sudt_amount(0) } }.stringify_keys
       end
       tx_generator = ckb_wallet.advance_generate(to_infos: to_infos)
       tx = ckb_wallet.advance_sign(tx_generator: tx_generator, contexts: Rails.application.credentials.OFFICIAL_WALLET_PRIVATE_KEY)
       tx_hash = api.send_transaction(tx)
-      claim_events.update_all(tx_hash: tx_hash, status: "processing")
+      values = claim_events.map do |claim_event|
+        { id: claim_event.id, tx_hash: tx_hash, status: "processing", created_at: claim_event.created_at, updated_at: Time.current }
+      end
+      ClaimEvent.upsert_all(values)
     end
   end
 end
--- app/services/update_claim_event_info_service.rb
+++ app/services/update_claim_event_info_service.rb
@@ -9,11 +9,11 @@ class UpdateClaimEventInfoService
       transaction_with_status = api.get_transaction(tx_hash)
       next if transaction_with_status.blank?
 
-      tx_status = transaction_with_status.status
+      tx_status = transaction_with_status.tx_status.status
       claim_event_status = tx_status == "committed" ? "processed" : claim_event.status
-      values << { id: claim_event.id, status: claim_event_status, tx_status: tx_status }
+      values << { id: claim_event.id, status: claim_event_status, tx_status: tx_status, created_at: claim_event.created_at, updated_at: Time.current }
     end
 
-    ClaimEvent.upsert_all(values, unique_by: :id)
+    ClaimEvent.upsert_all(values)
   end
 end
