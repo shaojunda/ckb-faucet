ac830ee1d47c5cdfb89622184704d76a834e0281
shaojunda <shaojunda@gmail.com>
2020-06-17 17:40:06 +0800

    fix: cell status check

--- app/services/check_output_service.rb
+++ app/services/check_output_service.rb
@@ -8,8 +8,9 @@ class CheckOutputService
 
     Output.where("block_number <= ?", safety_block_number).where(checked: false).dead.each do |output|
       out_point = CKB::Types::OutPoint.new(tx_hash: output.tx_hash, index: output.cell_index)
-      cell = api.get_live_cell(out_point)
-      output.live! if cell.present?
+      cell_with_status = api.get_live_cell(out_point)
+      output.live! if cell_with_status.cell.present?
+
       output.update(checked: true)
     end
 
