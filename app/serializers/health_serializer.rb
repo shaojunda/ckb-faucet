9bd86822732658b8e2fe117151f82f3bb3be5864
shaojunda <shaojunda@gmail.com>
2020-07-27 14:16:00 +0800

    chore: format code

--- app/serializers/health_serializer.rb
+++ app/serializers/health_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HealthSerializer
   include FastJsonapi::ObjectSerializer
   attributes :balance_state, :total_claim_state, :claim_per_product_state
--- test/factories/outputs.rb
+++ test/factories/outputs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :output do
     split_cell_event_id { "" }
