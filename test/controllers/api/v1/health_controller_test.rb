c83ad2c51232f345533b4d5d2d8040970ecf9c92
shaojunda <shaojunda@gmail.com>
2020-10-12 17:23:58 +0800

    chore: update cells_count

--- test/controllers/api/v1/health_controller_test.rb
+++ test/controllers/api/v1/health_controller_test.rb
@@ -3,7 +3,19 @@
 require "test_helper"
 
 class Api::V1::HealthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Account.create
+    Product.generate(name: "KFC", quota_config: { h24_quota: 10, h24_quota_per_request_type: 5 })
+  end
+
+  test "should return success" do
+    get api_v1_health_index_url
+
+    assert_equal 200, response.status
+  end
+
+  test "should return correct attributes" do
+    get api_v1_health_index_url
+    assert_equal %w[balance_state claim_per_product_state total_claim_state], json.dig("data", "attributes").keys.sort
+  end
 end
