51fc2a2d5ad9eaa4c48894ff5f7b79cd9252fa03
shaojunda <shaojunda@gmail.com>
2020-12-26 17:12:46 +0800

    test: only support new acp script

--- test/controllers/api/v1/claim_events_controller_test.rb
+++ test/controllers/api/v1/claim_events_controller_test.rb
@@ -405,4 +405,27 @@ class Api::V1::ClaimEventsControllerTest < ActionDispatch::IntegrationTest
 
     assert_equal response_json, response.body
   end
+
+  test "should return respond with error object when acp type is old" do
+    product = create(:product, quota_config: { "h24_quota": 4, "h24_quota_per_request_type": 2 })
+    type_script_args = "0x94bbc8327e16d195de87815c391e7b9131e80419c51a405a0b21227c6ee05129"
+    pk160 = "0x69b7667edbe08cf19413102fcadc53c67e34fb71"
+    request_body = { data: { id: 1, type: "claim_event", attributes: {
+      request_uuid: type_script_args, request_type: 0, pk160: pk160, acp_type: "old"
+    } } }.to_json
+    timestamp = Time.now.utc.strftime("%Y%m%dT%H%M%SZ")
+    request = mock
+    body = StringIO.new(request_body)
+    headers = { "x-ckbfs-date": timestamp, host: "domain.com" }.stringify_keys
+    request.expects(:headers).returns(headers).at_least_once
+    request.expects(:query_string).returns("").at_least_once
+    request.expects(:method).returns("POST").at_least_once
+    request.expects(:body).returns(body).at_least_once
+
+    valid_post api_v1_claim_events_url, params: request_body, headers: { "x-ckbfs-date": timestamp, "authorization": authorization(request, timestamp, product) }
+    error_object = Api::V1::ApiError::AcpTypeError.new
+    response_json = ApiErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    assert_equal response_json, response.body
+  end
 end
